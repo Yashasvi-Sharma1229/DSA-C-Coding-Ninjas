378c17910552710e0dff990366bc4da64dc62b58
Yashasvi-Sharma1229 <138844891+Yashasvi-Sharma1229@users.noreply.github.com>
2023-07-09 11:53:11 +0530

    Create Inverted Number Pattern.cpp
    Code : Inverted Number Pattern
    
    Print the following pattern for the given N number of rows.
    Pattern for N = 4
    4444
    333
    22
    1
    Input format :
    Integer N (Total no. of rows)
    Output format :
    Pattern in N lines
    Constraints :
    0 <= N <= 50
    Sample Input 1:
    5
    Sample Output 1:
    55555 
    4444
    333
    22
    1
    Sample Input 2:
    6
    Sample Output 2:
    666666
    55555 
    4444
    333
    22
    1

---
+++ Milestone 1/Patterns 2/Inverted Number Pattern.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+int n,i=1;
+    cin>>n;
+    while(i<=n){
+        int j=1;
+        while(j<=n-i+1){
+            cout<<n-i+1;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
+
+
